bb8ac5853787d38a0d23ceab3d9c2eba47840578
Xodjayev Muzaffar <94540363+Muzaffar-Xodjayev@users.noreply.github.com>
2024-06-25 15:59:37 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,11 +2,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.bot import DefaultBotProperties
-from database.models import db, models
 
 from data import config
 
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-db.create_tables(models)
